ba4d46de3028af6b27c88fcb5d0f35fc74bc7f8f
Juan Guana <juanc.guana@gmail.com>
2021-03-05 15:28:00 -0500

    feat: scrap with companies

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cheerio = require("cheerio");
 async function main(company) {
  const result = await request.get(`https://finviz.com/quote.ashx?t=${company}`);
  const $ = cheerio.load(result);
- $("body > div > div > table.snapshot-table2 > tbody > tr").each((index, element) => {
-   console.log($($(element).find("td")).text());
+ $("body > div > div > table.snapshot-table2 > tbody > tr > td").each((index, element) => {
+
+   console.log($($(element)).text());
  });
 }
  
-main('GOOG');
+main('DBTX');
